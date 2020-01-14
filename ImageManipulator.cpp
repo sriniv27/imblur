141f4ea17c447e505b2097ff19509575757dda0b
sugas182 <sugas182@gmail.com>
2020-01-13 20:29:20 -0500

    Move ctors to use initializer lists

--- ImageManipulator.cpp
+++ ImageManipulator.cpp
@@ -1,42 +1,13 @@
 #include "ImageManipulator.hpp"
 
 
-ImageManipulator::ImageManipulator(Mat inputImage)
-{
-  this->_imgIn = inputImage;
-  this->_imgOut = _imgIn.clone();
-
-  
-}
-
-ImageManipulator::ImageManipulator(Mat inputImage, int blurSize)
-{
-  this->_imgIn = inputImage;
-  this->_imgOut = _imgIn.clone();
-  this->_blurSize = blurSize;
-  
-}
-
-ImageManipulator::ImageManipulator(Mat inputImage, int blurSize, int numLoops)
-{
-  this->_imgIn = inputImage;
-  this->_imgOut = _imgIn.clone();
-  this->_blurSize = blurSize;
-  this->_numLoops = numLoops;
-}
-
-ImageManipulator::ImageManipulator()
-{
-    
-}
-
-
 ImageManipulator::~ImageManipulator()
 {
 }
 
-void ImageManipulator::blurFunction(Mat *inputImage,Mat *outputImage, int blursize){
-
-  GaussianBlur(*inputImage, *outputImage, Size(blursize,blursize), 1);
-  
+void ImageManipulator::blurFunction(){
+  auto imgIn = this->_imgIn;
+  auto imgOut = this->_imgOut;
+  auto blurSize = this->_blurSize;
+  GaussianBlur(imgIn, imgOut, Size(blurSize, blurSize), 1 );
 }
--- ImageManipulator.hpp
+++ ImageManipulator.hpp
@@ -6,17 +6,20 @@ class ImageManipulator
 {
 private:
   /* data */
-  
   Mat _imgIn;
   Mat _imgOut;
-  int _blurSize = 9;
-  int _numLoops = 3;
-  void blurFunction(Mat* inputImage, Mat* outputImage, int blurSize);
+  int _blurSize {9};
+  int _numLoops {3};
 public:
-ImageManipulator();
-  ImageManipulator(Mat inputImage);
-  ImageManipulator(Mat inputImage, int blurSize);
-  ImageManipulator(Mat inputImage, int blurSize, int numLoops);
+  void blurFunction();
+  Mat outputImage(){return this->_imgOut;};
+  ImageManipulator() = default;
+  ImageManipulator(Mat inputImage):ImageManipulator{inputImage,  _blurSize, _numLoops} {};
+  ImageManipulator(Mat inputImage, int blurSize) : ImageManipulator{inputImage, blurSize, _numLoops}{};
+  ImageManipulator(Mat inputImage, int blurSize, int numLoops): _imgIn{inputImage}{
+    this->_imgOut = this->_imgIn.clone();
+    this->_blurSize = blurSize;
+    this->_numLoops = numLoops;
+  }
   ~ImageManipulator();
-  
 };
