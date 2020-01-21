61e597bdf1fa12b82094c9f2e6d429b54c00cc79
sugas182 <sugas182@gmail.com>
2020-01-20 21:16:10 -0500

    update code to include spdlog

--- README.md
+++ README.md
@@ -8,14 +8,14 @@ The core program is outlined in main.cpp and provides the imblur executable.
 ## Installation
 
 ## Dependencies
-1) `imlib2` tp use the `imlib2_grab` function. Any drop-in replacement should be fine. We just need the image. 
+1) `imlib2` to use the `imlib2_grab` function. Any drop-in replacement should be fine. We just need the image. 
 2) `i3lock` or similar screen-locker that can accept an image to be used as background. See [`screen-lock`](./screen-lock) and replace `i3lock` with its associated syntax with your screen locker of choice.
 3) See [Dockerfile](./Dockerfile) for a minimal ubuntu installation example.
     - This project requires the following packages:
         - boost : `apt install libboost-all-dev`
         - OpenCV: `apt install libopencv-dev` 
 
-
-
-
+# This Project Uses: 
+## [spdlog](https://github.com/gabime/spdlog)
+A **fast**  c++ logging library. Install using your [package manager](https://github.com/gabime/spdlog#package-managers).
 ![build dependency graph](./doc/deps.png)
--- src/main.cpp
+++ src/main.cpp
@@ -1,131 +1,136 @@
 /**
  * @file main.cpp
  * @author Suhas (sugas182@gmail.com)
- * @brief Blur an image given its path and kernel size parameters 
+ * @brief Blur an image given its path and kernel size parameters
  * @version 0.1
  * @date 2020-01-19
- * 
+ *
  * @copyright Copyright (c) 2020
- * 
+ *
  */
-#include <iostream>
-#include <boost/stacktrace.hpp>
+#include "spdlog/spdlog.h" //https://github.com/gabime/spdlog
+
+#include "spdlog/sinks/stdout_color_sinks.h"
 #include <boost/program_options.hpp>
-#include <opencv2/imgproc.hpp>
+#include <boost/stacktrace.hpp>
+#include <iostream>
 #include <opencv2/core.hpp>
+#include <opencv2/core/utils/logger.hpp>
 #include <opencv2/highgui.hpp>
-
-
+#include <opencv2/imgproc.hpp>
 namespace po = boost::program_options;
- /// see here for doc: https://www.boost.org/doc/libs/1_64_0/doc/html/program_options.html
+
+/// see here for doc:
+/// https://www.boost.org/doc/libs/1_64_0/doc/html/program_options.html
 using namespace std;
 using namespace cv;
-
+void blurFunction(const Mat &imgIn, Mat &imgOut, const int &rows,
+                  const int &cols);
 /**
- * @brief Wrapper around cv::GaussianBlur that takes the address of the image 
- * being manipulated along with directed kernel matrix size paramter and applies 
- * a GaussianBlur on it.
- * 
- * @param imgIn 
- * @param imgOut 
- * @param rows 
- * @param cols 
- */
-void blurFunction(const Mat& imgIn, Mat& imgOut,const int& rows, const int& cols){
-auto ksize = Size(rows, cols);  
-  GaussianBlur(imgIn,imgOut , ksize, 1 );
-}
-
-
-/**
- * 
- * @brief imblur takes an image along with kernel size parameters and 
+ *
+ * @brief imblur takes an image along with kernel size parameters and
  * blurs it to be passed to a screen locker of your choice.
- * 
- * @param ac 
- * @param av 
- * @return int 
+ *
+ * @param ac
+ * @param av
+ * @return int
  */
-int main(int ac, char*av[]){
-  
-  try{
-  Mat image;
-  string filename;
-  string outfilename;
-  int rows=0;  
-  int cols=0;
-  bool quietOutput = false;
-  
-  po::options_description desc("helptext");
-  desc.add_options()
-    ("help,h","detailed help text")
-    ("input-file,i",po::value<string>(&filename),"Input file name")
-    ("output-file,o",po::value<string>(&outfilename)->default_value("output.png"),"Output file name")
-    ("rows,r",po::value<int>(&rows)->default_value(5),"number of rows in the smoothing kernel")
-    ("cols,c",po::value<int>(&cols)->default_value(3),"number of columns in the smoothing kernel")
-    ("quiet,q",po::value<bool>(&quietOutput)->default_value(false),"Quiet (no print to stdout)")
-    ; 
-  
-  po::variables_map vm;
-  po::store(po::parse_command_line(ac, av, desc), vm);
+int main(int ac, char *av[]) {
 
-  po::notify(vm);
-if(vm.count("help")){
+  auto console = spdlog::stdout_color_mt("console");
+  auto err_logger = spdlog::stderr_color_mt("stderr");
+  try {
+    Mat image;
+    string filename;
+    string outfilename;
+    int rows = 0;
+    int cols = 0;
+    bool quietOutput = false;
+
+    po::options_description desc("helptext");
+    desc.add_options()("help,h", "detailed help text")(
+        "input-file,i", po::value<string>(&filename), "Input file name")(
+        "output-file,o",
+        po::value<string>(&outfilename)->default_value("output.png"),
+        "Output file name")("rows,r", po::value<int>(&rows)->default_value(5),
+                            "number of rows in the smoothing kernel")(
+        "cols,c", po::value<int>(&cols)->default_value(3),
+        "number of columns in the smoothing kernel")(
+        "quiet,q", po::value<bool>(&quietOutput)->default_value(false),
+        "Quiet (no print to stdout)");
+
+    po::variables_map vm;
+    po::store(po::parse_command_line(ac, av, desc), vm);
+
+    po::notify(vm);
+    if (vm.count("help")) {
       cout << desc << "\n";
       return 1;
-    }    
-
-    if(vm.count("input-file")){
-      cout << "Input file: " << filename << "\n";
-    } else
-    {
-      cerr << "Input file not specified. Exiting" << "\n";
-     
     }
-    
-    
-    if(vm.count("quiet,q")){
+
+    if (vm.count("input-file")) {
+      cout << "Input file: " << filename << "\n";
+    } else {
+      cerr << "Input file not specified. Exiting"
+           << "\n";
+    }
+
+    if (vm.count("quiet,q")) {
       quietOutput = true;
     }
-    
-    
 
+    image = imread(
+        filename,
+        cv::ImreadModes::IMREAD_UNCHANGED); /// TODO: imread flags could
+                                            /// be turned into a user
+                                            /// configuration later maybe.
+    try {
+      if (!quietOutput) {
+        console->info("in filename: {}", boost::lexical_cast<string>(filename));
+        console->info("out filename: {}",
+                      boost::lexical_cast<string>(outfilename));
+      }
+    } catch (const spdlog::spdlog_ex &currentException) {
+      cerr << currentException.what() << "\n";
+    }
 
-image = imread(filename, cv::ImreadModes::IMREAD_UNCHANGED); ///TODO: imread flags could be turned into a user configuration later maybe. 
-  
-  if (!quietOutput){
-    cout<< "in filename: " << boost::lexical_cast<string>(filename)<<"\n";
-      cout<< "out filename: " << boost::lexical_cast<string>(outfilename)<<"\n";
-  
-  }
- auto ctr=0;
- Mat output_image_data;
+    auto ctr = 0;
+    Mat output_image_data;
 
-  blurFunction(image, image, rows, cols);
-  
-  
-  imwrite(outfilename, image);
-  return 0;
-  }
-  catch(po::error& e){
+    blurFunction(image, image, rows, cols);
+
+    imwrite(outfilename, image);
+    return 0;
+  } catch (po::error &e) {
     cerr << "Error in input: " << e.what() << "\n";
     cout << boost::stacktrace::stacktrace();
     return 1;
-  }
-  catch(cv::Exception& cvException){
-    cerr << "Runtime Error: \n\n" << cvException.msg << "\n";
- cerr << "ERROR CODE: " << cvException.code << "\n";
+  } catch (const spdlog::spdlog_ex &ex) {
+    cerr << "Runtime Error: \n\n" << ex.what() << "\n";
 
- cerr << boost::stacktrace::stacktrace();
-    return cvException.code;
-  }
-  catch(...){
-    cerr << "EXCEPTION UNKOWN!!";
-    
     cerr << boost::stacktrace::stacktrace();
     return 2;
-  }
-return 0;
+  } catch (...) {
+    cerr << "EXCEPTION UNKOWN!!";
 
+    cerr << boost::stacktrace::stacktrace();
+    return 3;
+  }
+  return 0;
 }
 
+/**
+ * @brief Wrapper around cv::GaussianBlur that takes the address of the image
+ * being manipulated along with directed kernel matrix size paramter and
+ * applies a GaussianBlur on it.
+ *
+ * @param imgIn
+ * @param imgOut
+ * @param rows
+ * @param cols
+ */
+void blurFunction(const Mat &imgIn, Mat &imgOut, const int &rows,
+                  const int &cols) {
+  auto ksize = Size(rows, cols);
+  GaussianBlur(imgIn, imgOut, ksize, 1);
+}
